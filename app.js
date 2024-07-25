21ae71115e5ecce9044cf7439452d02c33c7ad80
Hamza Baloch <158695894+Hamzabaloch08@users.noreply.github.com>
2024-07-26 03:01:22 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", mobileMenu);
 
-
 function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
