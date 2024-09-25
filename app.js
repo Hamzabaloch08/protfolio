9d8edba7dcfece42de423e811625446d09dd0564
Hamzabaloch08 <hb.developer08@gmail.com>
2024-09-25 18:17:10 +0500

    Update app.js

--- app.js
+++ app.js
@@ -8,5 +8,44 @@ function mobileMenu() {
     navMenu.classList.toggle("active");
 }
 
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
 
+const colors = [
+    "#1bb7b1", "#1ab8a3", "#1ac3b7", "#1bd8d0", "#1edfe3",
+    "#20e5e3", "#23e6e8", "#25e7e5", "#28eae3", "#2ae4e3",
+    "#2fe5e5", "#33e7e7", "#35e9e9", "#38f1f1", "#3cfbfb",
+    "#3ffbfb", "#3ffbfb", "#3ffbfb", "#3ffbfb"
+];
 
+circles.forEach(function (circle, index) {
+    circle.x = 0;
+    circle.y = 0;
+    circle.style.backgroundColor = colors[index % colors.length];
+});
+
+window.addEventListener("mousemove", function (e) {
+    coords.x = e.clientX;
+    coords.y = e.clientY;
+});
+
+function animationCircles() {
+    let x = coords.x;
+    let y = coords.y;
+
+    circles.forEach(function (circle, index) {
+        circle.style.left = x - 12 + "px";
+        circle.style.top = y - 12 + "px";
+
+        circle.style.transform = `scale(${(circles.length - index) / circles.length})`;
+
+        circle.x = x;
+        circle.y = y;
+
+        const nextCircle = circles[index + 1] || circles[0];
+        x += (nextCircle.x - x) * 0.3;
+        y += (nextCircle.y - y) * 0.3;
+    });
+    requestAnimationFrame(animationCircles);
+}
+animationCircles();
